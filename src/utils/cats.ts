3a3d4e3864d0794053d5770aa3364f3150d4d204
svetalana <sveta.sharymova@gmail.com>
2025-04-04 19:24:47 +0400

    fix getCats method add to get the same cats for product/id and product/id/edit pages

--- src/utils/cats.ts
+++ src/utils/cats.ts
@@ -1,7 +1,15 @@
 import { cache } from 'react';
 import type { Cat } from '@/types';
+import fs from 'fs';
+import path from 'path';
 
 export const getCats = cache(async () => {
+  const cacheFile = path.join(process.cwd(), 'cats-cache.json');
+  
+  if (fs.existsSync(cacheFile)) {
+    return JSON.parse(fs.readFileSync(cacheFile, 'utf-8'));
+  }
+
   console.log('getCats API call');
   const API_KEY = process.env.NEXT_PUBLIC_CAT_API_KEY;
   const API_URL = 'https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=25';
@@ -12,6 +20,9 @@ export const getCats = cache(async () => {
     }
   });
   const cats = await response.json();
+  
+  fs.writeFileSync(cacheFile, JSON.stringify(cats));
+  
   console.log('getCats API call response', cats.map((cat: Cat) => cat.id));
   return cats;
 }); 
